31c62cc3cfbb06be761ccd3d80f951eb2b5b7a0c
bvillela <brunovillela@MacBook-Pro-de-Bruno.local>
2015-06-03 21:15:37 -0400

    My first app

---
+++ Classwork/Lesson 01/Lesson1/Lesson1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lesson1
+//
+//  Created by Bruno Villela on 6/3/15.
+//  Copyright (c) 2015 Bruno Villela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
